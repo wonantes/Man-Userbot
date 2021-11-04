5729b2bc0ba1515279e9dd2c99301788d0cd7177
wonantes <86704064+wonantes@users.noreply.github.com>
2021-11-05 04:06:29 +0700

    Update wordcloud.py

--- userbot/modules/wordcloud.py
+++ userbot/modules/wordcloud.py
@@ -7,7 +7,15 @@
 import os
 
 import numpy as np
+import sys
+import traceback
+import time
+import random
+import re
 from hachoir.metadata import extractMetadata
+from telethon.sync import TelegramClient
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty, InputPeerChannel, InputPeerUser
 from hachoir.parser import createParser
 from PIL import Image
 from scipy.ndimage import gaussian_gradient_magnitude
@@ -19,85 +27,53 @@ from userbot import CMD_HELP, bot
 from userbot.events import man_cmd
 
 
-@bot.on(man_cmd(outgoing=True, pattern=r"(wc)$"))
-async def _(event):
-    if not event.reply_to_msg_id:
-        await event.edit("`Mohon Balas Ke Media Apapun`")
-        return
-    reply_message = await event.get_reply_message()
-    if not reply_message.media:
-        await event.edit("`Mohon Balas Ke Gambar/Sticker/Video`")
-        return
-    await event.edit("`Mendownload Media.....`")
-    if reply_message.photo:
-        await bot.download_media(
-            reply_message,
-            "wc.png",
-        )
-    elif (
-        DocumentAttributeFilename(file_name="AnimatedSticker.tgs")
-        in reply_message.media.document.attributes
-    ):
-        await bot.download_media(
-            reply_message,
-            "wc.tgs",
-        )
-        os.system("lottie_convert.py wc.tgs wc.png")
-    elif reply_message.video:
-        video = await bot.download_media(
-            reply_message,
-            "wc.mp4",
-        )
-        extractMetadata(createParser(video))
-        os.system("ffmpeg -i wc.mp4 -vframes 1 -an -s 480x360 -ss 1 wc.png")
-    else:
-        await bot.download_media(
-            reply_message,
-            "wc.png",
-        )
+api_id = 8857087  # API ID
+api_hash = '55f503a96cfdbac2d30d752d47d5a049'  #API HASH
+phone = '+6285866508459'  #NUMBER WITH COUNTRY CODE
+timer = 120 #TIME TO WAIT BEFORE NEXT SENDING
+msgtosend = "Free Shill 1 Day contact @AmaMarketing" # MESSAGE TO SEND
+client = TelegramClient(phone, api_id, api_hash)
+client.connect()
+if not client.is_user_authorized():
+    client.send_code_request(phone)
+    client.sign_in(phone, input('Enter the code: '))
+chats = []
+last_date = None
+chunk_size = 10
+groups=[]
+result = client(GetDialogsRequest(
+                offset_date=last_date,
+                offset_id=0,
+                offset_peer=InputPeerEmpty(),
+                limit=chunk_size,
+                hash = 0
+            ))
+chats.extend(result.chats)
+for chat in chats:
     try:
-        await event.edit("`Sedang Memproses....`")
-        text = open("userbot/utils/styles/alice.txt", encoding="utf-8").read()
-        image_color = np.array(Image.open("wc.png"))
-        image_color = image_color[::1, ::1]
-        image_mask = image_color.copy()
-        image_mask[image_mask.sum(axis=2) == 0] = 255
-        edges = np.mean(
-            [
-                gaussian_gradient_magnitude(image_color[:, :, i] / 255.0, 2)
-                for i in range(3)
-            ],
-            axis=0,
-        )
-        image_mask[edges > 0.08] = 255
-        wc = WordCloud(
-            max_words=2000,
-            mask=image_mask,
-            max_font_size=40,
-            random_state=42,
-            relative_scaling=0,
-        )
-        wc.generate(text)
-        image_colors = ImageColorGenerator(image_color)
-        wc.recolor(color_func=image_colors)
-        wc.to_file("wc.png")
-        await event.client.send_file(
-            event.chat_id,
-            "wc.png",
-            reply_to=event.reply_to_msg_id,
-        )
-        await event.delete()
-        os.system("rm *.png *.mp4 *.tgs *.webp")
-    except BaseException as e:
-        os.system("rm *.png *.mp4 *.tgs *.webp")
-        return await event.edit(str(e))
-
-
-CMD_HELP.update(
-    {
-        "wordcloud": f"**Plugin : **`wordcloud`\
-        \n\n  •  **Syntax :** `{cmd}wc`\
-        \n  •  **Function : **membuat seni wordcloud dari media.\
-    "
-    }
-)
+        groups.append(chat)
+    except:
+        continue
+while True:    
+    for group in groups:
+        try:
+            username1 = group.username
+            print(username1)    
+            client.send_message(username1, msgtosend)
+            time.sleep(1)
+        except:
+            continue 
+    print("Send Complete!!, Waiting for " + str(timer) + " seconds")
+    time.sleep(timer)
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
